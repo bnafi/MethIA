d2dab074650bc1a03d2c5ded257d22dc4918ba84
AngelReyero <anreyero@ucm.es>
2023-03-13 18:21:16 +0100

    Docstrings and cleaned version.

--- gradient_descent.py
+++ gradient_descent.py
@@ -19,7 +19,9 @@ import image_statistics
 import loss_function
 import utils
 
-"""This File will compute the Gradient Descent to update the image to improve the loss given by loss_function"""
+"""This File will compute the Gradient Descent to update the image to improve the loss given by loss_function.
+  The method optimization will compute the output image. 
+"""
 
 
 
@@ -42,8 +44,25 @@ def train_step(image, opt, extractor, style_targets, content_targets, style_weig
   opt.apply_gradients([(grad, image)])
   image.assign(clip_0_1(image))
 
-def optimization(content_image, style_image, extractor, style_targets, content_targets, style_layers, content_layers,epochs=5, steps_per_epoch=100):
-    image = tf.Variable(content_image)#We will begin by the content_image
+def optimization(init_image,content_image, style_image,  style_layers, content_layers,epochs=5, steps_per_epoch=100):
+    """
+    This is the function that will take an input image and will generate an output image that will minimize the loss function,
+    parametrized by some weightening.
+
+    Inputs:
+    -init_image: the first image from where we will begin. (Normally content_image)
+    -content_image: the image from which we will compute the content loss.
+    -style_image:the image from which we will compute the style loss.
+    -style_layers: the VGG19 layers that we will use to compute the style loss
+    -content_layers: the VGG19 layers that we will use to compute the content loss
+    
+
+    """
+    #extractor: it is the network that will extract the style and the content from the images
+    extractor = image_statistics.StyleContentModel(style_layers, content_layers)
+    style_targets = extractor(style_image)['style']
+    content_targets = extractor(content_image)['content']
+    image = tf.Variable(init_image) #Normally, we will begin by the content_image
     opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
     style_weight=1e-2
     content_weight=1e4
@@ -56,8 +75,9 @@ def optimization(content_image, style_image, extractor, style_targets, content_t
             train_step(image, opt, extractor, style_targets, content_targets, style_weight, content_weight, style_layers, content_layers)
             print(".", end='', flush=True)
     display.clear_output(wait=True)
-    display.display(utils.tensor_to_image(image))
+    display.display(utils.tensor_to_image(image))#This is not for the terminal, it is to use it in streamlit or in jupyter to show the output image
     print("Train step: {}".format(step))
 
     end = time.time()
     print("Total time: {:.1f}".format(end-start))
+    return utils.tensor_to_image(image)
--- main.py
+++ main.py
@@ -19,22 +19,24 @@ import loss_function
 import gradient_descent
 import image_statistics
 
+"""
+This file gives just an example of the use of the architecture. 
+We use the gradient descent to optimize our initial image
+
+If we want to change the content image and style image we would have to change the URLs given.
+
+"""
+
 content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
 style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
 content_image = utils.load_img(content_path)
 style_image = utils.load_img(style_path)
-utils.imshow(style_image)
+utils.imshow(style_image)#Not for terminal
 style_layers = ['block1_conv1',
                 'block2_conv1',
                 'block3_conv1', 
                 'block4_conv1', 
                 'block5_conv1']
 content_layers = ['block5_conv2']
-#loss_function.vgg_layers_stat(style_image, style_layers)
-extractor = image_statistics.StyleContentModel(style_layers, content_layers)
-results = extractor(tf.constant(content_image))
-extractor = image_statistics.StyleContentModel(style_layers, content_layers)
-style_targets = extractor(style_image)['style']
-content_targets = extractor(content_image)['content']
 
-gradient_descent.optimization(content_image, style_image, extractor, style_targets, content_targets, style_layers, content_layers,epochs=2, steps_per_epoch=10)
+gradient_descent.optimization(content_image,content_image, style_image, style_layers, content_layers,epochs=2, steps_per_epoch=10)
